1deb50a2b45ba760d159528b31cd1b4ffc3a8f93
Gary Kwong [:gkw] <nth10sd@gmail.com>
2024-08-19 02:02:30 -0700

    Bump to Sphinx 8.0.2 and update corresponding settings

--- docs/gen-sphinx-html.sh
+++ docs/gen-sphinx-html.sh
@@ -2,7 +2,7 @@
 
 # Bootstrap Sphinx
 sphinx-quickstart --sep -p "startrepo" -a "FOO" -r "BAR" -l "en" \
-    --ext-autodoc --no-makefile --no-batchfile --no-use-make-mode;
+    --ext-autodoc --no-makefile --no-batchfile;
 
 # Copy over the intended copy of conf.py
 cp source/conf_correct.py source/conf.py;
@@ -10,10 +10,17 @@ cp source/conf_correct.py source/conf.py;
 # Generate Sphinx documentation
 sphinx-apidoc -o source/ ../startrepo/;
 
-# Add "modules" to portion of index.rst in the middle of several empty lines
-tr '\n' '\f' < source/index.rst | \
-    sed 's/\f\f\f\f/\f\f   modules\f\f/' | tr '\f' '\n' > source/index_new.rst;
-mv source/index_new.rst source/index.rst;
+# Add "modules" and "Indices and tables" section
+cat << "EOF" >> source/index.rst
+   modules
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+EOF
 
 # Generate Sphinx HTML documentation
 sphinx-build -W --keep-going -b html source/ build/html/;
