40002f23516ae64123c53c56f08d7b840506ab65
Gary Kwong [:gkw] <nth10sd@gmail.com>
2025-06-01 12:11:24 -0700

    Remove beartype for now due to poor support for dataclasses

--- startrepo/__init__.py
+++ startrepo/__init__.py
@@ -1,18 +1,19 @@
 """Module details."""
 
-import contextlib
 from pathlib import Path
 
-with contextlib.suppress(ModuleNotFoundError):  # Bypass package install issues on CI
-    from beartype import BeartypeConf
-    from beartype.claw import beartype_this_package
+# \with contextlib.suppress(ModuleNotFoundError):  # Bypass package install issues on CI
+# \    from beartype import BeartypeConf
+# \    from beartype.claw import beartype_this_package
 
 __title__ = "startrepo"
 __version__ = (Path(__file__).parent / "_version.txt").read_text(encoding="utf-8")
 
-with contextlib.suppress(NameError):
-    beartype_this_package(  # pyright: ignore[reportPossiblyUnboundVariable]
-        conf=BeartypeConf(  # pyright: ignore[reportPossiblyUnboundVariable]
-            violation_type=UserWarning
-        )
-    )
+# Beartype does not support dataclasses well yet, see:
+# https://github.com/beartype/beartype/issues/119
+# \with contextlib.suppress(NameError):
+# \    beartype_this_package(
+# \        conf=BeartypeConf(
+# \            violation_type=UserWarning
+# \        )
+# \    )
