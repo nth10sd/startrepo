c1dff1b1c71ca8f22ee2dd7a1757574b2e7f8997
Gary Kwong [:gkw] <nth10sd@gmail.com>
2025-06-01 12:29:59 -0700

    Reactivate TC001, TC002 and TC003 checking by ruff

--- startrepo/__init__.py
+++ startrepo/__init__.py
@@ -9,6 +9,8 @@ from pathlib import Path
 __title__ = "startrepo"
 __version__ = (Path(__file__).parent / "_version.txt").read_text(encoding="utf-8")
 
+# If reactivation of beartype is desired, disable ruff TC001, TC002 and TC003 errors
+# "TC001",   # TYPE_CHECKING seems incompatible with beartype
 # Beartype does not support dataclasses well yet, see:
 # https://github.com/beartype/beartype/issues/119
 # \with contextlib.suppress(NameError):
